57c98e8b40f9da62d8307a27b8847b1300f80629
MFaizanAli-24 <mfaizanali008@gmail.com>
2026-08-11 03:14:37 -0400

    Added fix for feature: combine part numbers quotes for the same email

--- app/main.py
+++ app/main.py
@@ -168,7 +168,7 @@ def main() -> None:
     
 
     logger.info("==================== INDIVIDUAL QUOTE EMAILS ====================")
-    for match in matches:
+    for match in aggregated_matches:
         logger.info("--"*30)
         send_quote_email(match)
         logger.info("--"*30)
